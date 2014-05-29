383501690ad04a724dab2202fcaecadaf44b43f4
Ajnas <mailajnaskt@gmail.com>
2014-05-29 19:38:41 +0530

    Registered Custom User in Admin Page

--- leave/admin.py
+++ leave/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+from leave.models import UserProfile
+
+# Define an inline admin descriptor for Employee model
+# which acts a bit like a singleton
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    
+
+# Define a new User admin
+class UserAdmin(UserAdmin):
+    inlines = (UserProfileInline, )
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
