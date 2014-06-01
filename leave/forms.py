1721b9cb89f5d397b0aaa5223e857293633fb681
Ajnas <mailajnaskt@gmail.com>
2014-06-01 22:37:29 +0530

    Change reason field to TextArea

--- leave/forms.py
+++ leave/forms.py
@@ -4,6 +4,9 @@ from leave.models import Application,Employee
 
 
 class ApplicationForm(ModelForm):
+
+	
+
 	class Meta:
    		model = Application
    		fields = ['employee', 'leave_type', 'date_from', 'date_to','reason','attachments']
--- leave/models.py
+++ leave/models.py
@@ -94,7 +94,7 @@ class Application(models.Model):
 	date_to	= models.DateField()
 	status = models.IntegerField(choices=STATUS,default=1)
 	current_position= models.IntegerField(choices=USER_TYPES,default=1)
-	reason = models.CharField(max_length=200)
+	reason = models.TextField(max_length=200)
 	attachments = models.CharField(max_length=200,blank=True)
 	time_generated = models.DateTimeField(auto_now_add=True)
 	time_apporoved = models.DateTimeField(null=True,blank=True)	#This field will be set only when the application is received.
