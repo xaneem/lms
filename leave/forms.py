06727605173d21137019b7dfe11c45c6b20520d7
Ajnas <mailajnaskt@gmail.com>
2014-06-01 19:49:52 +0530

    Form validation

--- leave/forms.py
+++ leave/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from datetime import datetime 
 from leave.models import Application,Employee
 
 
@@ -6,6 +7,34 @@ class ApplicationForm(ModelForm):
 	class Meta:
    		model = Application
    		fields = ['employee', 'leave_type', 'date_from', 'date_to','reason','attachments']
-	 
+	
+	def is_valid(self):
+		valid=super(ApplicationForm,self).is_valid()
+		if not valid:
+			return valid
+
+		date_from=self.cleaned_data['date_from']
+		date_to=self.cleaned_data['date_to']
+		leave_balance=self.cleaned_data['employee'].leave_balance
+		if date_from<datetime.now().date():
+			self._errors['date_from']="Aaha?"
+			return False
+
+		if(date_to<date_from):
+			self._errors['date_to']="Seriously? :P"
+			return False
+
+		
+
+		if((date_to-date_from).days>leave_balance):
+			self._errors['date_to']="Insufficient leave balance"
+			return False
+
+
+		return True
+
+
+
+
 
 
